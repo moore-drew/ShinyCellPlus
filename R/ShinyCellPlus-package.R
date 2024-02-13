ed4d66feffa438ddbfaa9ad252f25300a3d35bf9
Drew Moore <35266356+moore-drew@users.noreply.github.com>
2024-02-13 12:20:58 -0500

    updating library dependencies

---
+++ R/ShinyCellPlus-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr filter
+#' @importFrom dplyr rename
+#' @importFrom GSEABase GeneSet
+#' @importFrom stringr str_sub
+## usethis namespace: end
+NULL
